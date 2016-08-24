6af7ffd2d16cf1dada01bbbbd884fb6e4a1b9701
Warren Schrader <wwschrader@gmail.com>
2016-08-23 21:00:27 -0700

    Add database table

---
+++ app/src/main/java/com/example/android/peakfresh/data/ProductColumns.java
@@ -0,0 +1,22 @@
+package com.example.android.peakfresh.data;
+
+import net.simonvt.schematic.annotation.AutoIncrement;
+import net.simonvt.schematic.annotation.DataType;
+import net.simonvt.schematic.annotation.NotNull;
+import net.simonvt.schematic.annotation.PrimaryKey;
+
+/**
+ * Created by Warren on 8/23/2016.
+ */
+public interface ProductColumns {
+
+    @DataType(DataType.Type.INTEGER) @PrimaryKey @AutoIncrement String _ID = "_id";
+
+    @DataType(DataType.Type.TEXT) @NotNull String PRODUCT_NAME = "product_name";
+
+    @DataType(DataType.Type.TEXT) @NotNull String PRODUCT_EXPIRATION_DATE = "product_expiration_date";
+
+    @DataType(DataType.Type.TEXT) @NotNull String PRODUCT_CATEGORY = "product_category";
+
+
+}
--- app/src/main/java/com/example/android/peakfresh/ui/MainActivity.java
+++ app/src/main/java/com/example/android/peakfresh/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.android.peakfresh;
+package com.example.android.peakfresh.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.android.peakfresh.R;
 
 public class MainActivity extends AppCompatActivity {
 
