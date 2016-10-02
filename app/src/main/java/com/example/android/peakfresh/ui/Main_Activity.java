b7a8d09388f8b06e1b2f635e44d8da13fed2dd86
Warren Schrader <wwschrader@gmail.com>
2016-10-01 18:00:12 -0700

    Change theme and added settings menu button to main activity

--- app/src/main/java/com/example/android/peakfresh/ui/Main_Activity.java
+++ app/src/main/java/com/example/android/peakfresh/ui/Main_Activity.java
@@ -2,6 +2,8 @@ package com.example.android.peakfresh.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.android.peakfresh.R;
 
@@ -11,6 +13,8 @@ public class Main_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         //Checking if activity is using fragment container
         if (findViewById(R.id.fragment_main_container) != null) {
@@ -28,6 +32,16 @@ public class Main_Activity extends AppCompatActivity {
                     .add(R.id.fragment_main_container, mainFragmentList).commit();
         }
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
